e3d4cac26a3ff50d2fcfb04171a4b3b313c3aea1
santiago atrio <299792458ignali2021@gmail.com>
2023-03-21 12:22:04 -0300

    agrego preguntas

---
+++ sombrero_seleccionador.py
@@ -0,0 +1,32 @@
+""""
+ * Crea un programa que simule el comportamiento del sombrero selccionador del
+ * universo mágico de Harry Potter.
+ * - De ser posible realizará 5 preguntas (como mínimo) a través de la terminal.
+ * - Cada pregunta tendrá 4 respuestas posibles (también a selecciona una a través de terminal).
+ * - En función de las respuestas a las 5 preguntas deberás diseñar un algoritmo que
+ *   coloque al alumno en una de las 4 casas de Hogwarts:
+ *   (Gryffindor, Slytherin , Hufflepuff y Ravenclaw)
+ * - Ten en cuenta los rasgos de cada casa para hacer las preguntas
+ *   y crear el algoritmo seleccionador:
+ *   Por ejemplo, en Slytherin se premia la ambición y la astucia.
+ """
+
+"""
+ 5 inputs con 4 resp 
+gryp = valientes
+slythe = ambicion y astucia
+huff = apoyo
+raven  = inteligentes
+"""
+
+#PREGUNTAS
+preg1 = f"{input("Ves que una persona está siendo atacada por un dementor ¿que harías?")}"  #\n 1-peleo con dementor \n 2-dejo que ataquen a la persona para escapar \n 3-ayudo persona para q me ayuder a enfrentarlo\n 4-casteo hechizo como teletransportación"
+preg2 = f"{input("ves a una persona llorando")"\n 1-le digo q le pasa y ayudo a resolver el preblema \n 2-no hago nada \n 3-le doy apoyo emocional \n 4-le doy una respues para aliviar su tristeza"}"
+preg3 = f"{input("ves un animal en la calle") "\n 1-trato de encontrar de quien es \n 2-no hago nada \n 3-lo llevo a casa y cuido hasta que se mejore \n 4-hago una publicidad para encontrar al dueño más facilmente"} 
+
+#preg3 = f"{input("")}"
+
+
+#SOMBRERO SELECCIONADOR
+
+
