5ee6aa63145c8839d019901c8f2cbd012131cbf7
santiago atrio <299792458ignali2021@gmail.com>
2023-03-21 15:14:20 -0300

    terminé

--- sombrero_seleccionador.py
+++ sombrero_seleccionador.py
@@ -13,21 +13,19 @@
 
 
 #PREGUNTAS
-preg1 = input("Ves que una persona está siendo atacada por un dementor ¿que harías? \n 1-peleo con dementor \n 2-dejo que ataquen a la persona para escapar \n 3-ayudo persona para q me ayuder a enfrentarlo\n 4-casteo hechizo como teletransportación\nRespuesta: ") 
-preg2 = input("ves a una persona llorando \n 1-le digo q le pasa y ayudo a resolver el preblema \n 2-no hago nada \n 3-le doy apoyo emocional \n 4-le doy una respues para aliviar su tristeza\nRespuesta: ") 
-preg3 = input("ves un animal en la calle \n 1-trato de encontrar de quien es \n 2-no hago nada \n 3-lo llevo a casa y cuido hasta que se mejore \n 4-hago una publicidad para encontrar al dueño más facilmente\nRespuesta: ") 
-preg4 = input("¿qué animal te gusta más? \n 1-un grifo \n 2-una serpiente \n 3-un oso panda \n 4-un cuervo\nRespuesta: ")
-preg5 = input("describite a ti mismo \n 1-soy valiente \n 2-soy ambicionso \n 3-soy amable \n 4-me gusta estudiar\nRespuesta: ")
+preg1 = int(input("Ves que una persona está siendo atacada por un dementor ¿que harías? \n 1-peleo con dementor \n 2-dejo que ataquen a la persona para escapar \n 3-ayudo persona para q me ayuder a enfrentarlo\n 4-casteo hechizo como teletransportación\nRespuesta: ")) 
+preg2 = int(input("ves a una persona llorando \n 1-le digo q le pasa y ayudo a resolver el preblema \n 2-no hago nada \n 3-le doy apoyo emocional \n 4-le doy una respues para aliviar su tristeza\nRespuesta: ") )
+preg3 = int(input("ves un animal en la calle \n 1-trato de encontrar de quien es \n 2-no hago nada \n 3-lo llevo a casa y cuido hasta que se mejore \n 4-hago una publicidad para encontrar al dueño más facilmente\nRespuesta: ")) 
+preg4 = int(input("¿qué animal te gusta más? \n 1-un grifo \n 2-una serpiente \n 3-un oso panda \n 4-un cuervo\nRespuesta: "))
+preg5 = int(input("describite a ti mismo \n 1-soy valiente \n 2-soy ambicionso \n 3-soy amable \n 4-me gusta estudiar\nRespuesta: "))
 
 
 #SOMBRERO SELECCIONADOR
 lista_de_preguntas = [preg1,preg2,preg3,preg4,preg5]
-
+contador = 0    
+casa_ganadora = 0
 
 for i in [1,2,3,4]:
-    contador = 0
-    casa_ganadora = 0
-    print(lista_de_preguntas.count(i))
 
     if contador < lista_de_preguntas.count(i):
         contador = lista_de_preguntas.count(i)  
@@ -35,12 +33,14 @@ for i in [1,2,3,4]:
 
 print(contador)
 print(casa_ganadora)
-#elijo casa 
-print
-if   i==1:  print("Griffindor")
+#elijo casa
+ 
 
-elif i==2:  print("Slytherin")
-
-elif i==3:  print("Hufflepuff")
-
-elif i==4 :  print("Ravenclaw")
+if   casa_ganadora==1:  
+    print("Tu casa será Griffindor")
+elif casa_ganadora==2:
+    print("Tu casa será Slytherin")
+elif casa_ganadora==3:  
+    print("Tu casa será Hufflepuff")
+elif casa_ganadora==4 : 
+    print("Tu casa será Ravenclaw")
