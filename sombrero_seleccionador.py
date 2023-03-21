63f9bc6358eac7f73652196c7231f1d759c2b354
santiago atrio <299792458ignali2021@gmail.com>
2023-03-21 14:39:57 -0300

    termiandas las preguntas

--- sombrero_seleccionador.py
+++ sombrero_seleccionador.py
@@ -11,20 +11,13 @@
  *   Por ejemplo, en Slytherin se premia la ambición y la astucia.
  """
 
-"""
- 5 inputs con 4 resp 
-gryp = valientes
-slythe = ambicion y astucia
-huff = apoyo
-raven  = inteligentes
-"""
 
 #PREGUNTAS
-preg1 = f"{input("Ves que una persona está siendo atacada por un dementor ¿que harías?")}"  #\n 1-peleo con dementor \n 2-dejo que ataquen a la persona para escapar \n 3-ayudo persona para q me ayuder a enfrentarlo\n 4-casteo hechizo como teletransportación"
-preg2 = f"{input("ves a una persona llorando")"\n 1-le digo q le pasa y ayudo a resolver el preblema \n 2-no hago nada \n 3-le doy apoyo emocional \n 4-le doy una respues para aliviar su tristeza"}"
-preg3 = f"{input("ves un animal en la calle") "\n 1-trato de encontrar de quien es \n 2-no hago nada \n 3-lo llevo a casa y cuido hasta que se mejore \n 4-hago una publicidad para encontrar al dueño más facilmente"} 
-
-#preg3 = f"{input("")}"
+preg1 = input("Ves que una persona está siendo atacada por un dementor ¿que harías? \n 1-peleo con dementor \n 2-dejo que ataquen a la persona para escapar \n 3-ayudo persona para q me ayuder a enfrentarlo\n 4-casteo hechizo como teletransportación\n") 
+preg2 = input("ves a una persona llorando \n 1-le digo q le pasa y ayudo a resolver el preblema \n 2-no hago nada \n 3-le doy apoyo emocional \n 4-le doy una respues para aliviar su tristeza") 
+preg3 = input("ves un animal en la calle \n 1-trato de encontrar de quien es \n 2-no hago nada \n 3-lo llevo a casa y cuido hasta que se mejore \n 4-hago una publicidad para encontrar al dueño más facilmente") 
+preg4 = input("¿qué animal te gusta más? \n 1-un grifo \n 2-una serpiente \n 3-un oso panda \n 4-un cuervo")
+preg5 = input("describite a ti mismo \n 1-soy valiente \n 2-soy ambicionso \n 3-soy amable \n 4- me gusta estudiar")
 
 
 #SOMBRERO SELECCIONADOR
